205e0081f119163c3374a4596a644b4957493ceb
Astarose <baptiste.regnier62@gmail.com>
2023-09-30 16:44:42 +0200

    élément news vue desktop + début mobile

--- src/components/qFooter.vue
+++ src/components/qFooter.vue
@@ -3,124 +3,128 @@
     class="col-12 q-pa-lg"
     style="height: 390px; background-color: black; color: white"
   >
-    <div class="row">
-      <div class="col-4">
-        <div style="font-size: 32.8px">Lorem ipsum</div>
-        <div style="font-size: 16px" v-show="!viewScreen">
-          <div>we are not here to sell you products,</div>
-          <div>we sell value through our experience</div>
+    <div>
+      <div class="row">
+        <div class="col-4 flex">
+          <div style="font-size: 32.8px">Lorem ipsum</div>
+          <div style="font-size: 16px" v-show="!viewScreen">
+            <div>
+              we are not here to sell you products,<br />we sell value through
+              our experience
+            </div>
+          </div>
+          <div class="row flex align-end q-mt-xl">
+            <q-img
+              class="q-ma-sm"
+              src="src/assets/logoFacebook.svg"
+              style="height: 24px; width: 24px"
+            ></q-img>
+            <q-img
+              class="q-ma-sm"
+              src="src/assets/logoTwitter.svg"
+              style="height: 24px; width: 24px"
+            ></q-img>
+            <q-img
+              class="q-ma-sm"
+              src="src/assets/logoInstagram.svg"
+              style="height: 24px; width: 24px"
+            ></q-img>
+            <q-img
+              class="q-ma-sm"
+              src="src/assets/logoLinkedin.svg"
+              style="height: 24px; width: 24px"
+            ></q-img>
+          </div>
+        </div>
+        <div class="col-8">
+          <div class="flex">
+            <q-list style="width: fit-content">
+              <q-item clickable>
+                <q-item-section> Home </q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section> Courses </q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section> Instructors </q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section> Resources </q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section> Contact us </q-item-section>
+              </q-item>
+            </q-list>
+            <q-list style="width: fit-content">
+              <q-item clickable>
+                <q-item-section> SME </q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section> Solution </q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section> Reviews </q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section> Blog </q-item-section>
+              </q-item>
+            </q-list>
+            <q-list style="width: fit-content">
+              <q-item clickable>
+                <q-item-section> Contact Us </q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section> Place A Call </q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section> Email </q-item-section>
+              </q-item>
+            </q-list>
+            <q-list style="width: fit-content" v-show="!viewScreen">
+              <q-item clickable>
+                <q-item-section> Job Opening </q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section> News </q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section> Research </q-item-section>
+              </q-item>
+            </q-list>
+            <q-list style="width: fit-content" v-show="!viewScreen">
+              <q-item clickable>
+                <q-item-section> UK Privacy Policy </q-item-section>
+              </q-item>
+              <q-item clickable>
+                <q-item-section> Terms Of Use </q-item-section>
+              </q-item>
+            </q-list>
+          </div>
         </div>
       </div>
-      <div class="col-8">
-        <div class="flex">
-          <q-list style="width: fit-content">
-            <q-item clickable>
-              <q-item-section> Home </q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section> Courses </q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section> Instructors </q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section> Resources </q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section> Contact us </q-item-section>
-            </q-item>
-          </q-list>
-          <q-list style="width: fit-content">
-            <q-item clickable>
-              <q-item-section> SME </q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section> Solution </q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section> Reviews </q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section> Blog </q-item-section>
-            </q-item>
-          </q-list>
-          <q-list style="width: fit-content">
-            <q-item clickable>
-              <q-item-section> Contact Us </q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section> Place A Call </q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section> Email </q-item-section>
-            </q-item>
-          </q-list>
-          <q-list style="width: fit-content" v-show="!viewScreen">
-            <q-item clickable>
-              <q-item-section> Job Opening </q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section> News </q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section> Research </q-item-section>
-            </q-item>
-          </q-list>
-          <q-list style="width: fit-content" v-show="!viewScreen">
-            <q-item clickable>
-              <q-item-section> UK Privacy Policy </q-item-section>
-            </q-item>
-            <q-item clickable>
-              <q-item-section> Terms Of Use </q-item-section>
-            </q-item>
-          </q-list>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <q-img
-        class="q-ma-sm"
-        src="src/assets/logoFacebook.svg"
-        style="height: 24px; width: 24px"
-      ></q-img>
-      <q-img
-        class="q-ma-sm"
-        src="src/assets/logoTwitter.svg"
-        style="height: 24px; width: 24px"
-      ></q-img>
-      <q-img
-        class="q-ma-sm"
-        src="src/assets/logoInstagram.svg"
-        style="height: 24px; width: 24px"
-      ></q-img>
-      <q-img
-        class="q-ma-sm"
-        src="src/assets/logoLinkedin.svg"
-        style="height: 24px; width: 24px"
-      ></q-img>
-    </div>
-    <div class="row">
-      <div class="col-12 flex flex-center q-gutter-y-md">
-        <div class="full-width flex justify-center">
-          Subscribe to get latest updates
-        </div>
-        <div class="flex justify-center full-width">
-          <q-input
-            filled
-            outlined
-            :style="{ width: viewScreen ? '80%' : '30%' }"
-            dense
-            placeholder="Your Email address"
-            class="custom-input"
-          />
+      <div class="row">
+        <div class="col-12 flex flex-center q-gutter-y-md">
+          <div class="full-width flex justify-center">
+            Subscribe to get latest updates
+          </div>
+          <div class="flex justify-center full-width">
+            <q-input
+              filled
+              outlined
+              :style="{ width: viewScreen ? '80%' : '30%' }"
+              dense
+              placeholder="Your Email address"
+              class="custom-input"
+            />
 
-          <q-btn
-            square
-            no-caps
-            flat
-            label="Submit"
-            style="background-color: #96c67f"
-          />
+            <q-btn
+              square
+              no-caps
+              flat
+              label="Submit"
+              style="background-color: #96c67f"
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,183 @@
 <template>
-  <q-page class="flex flex-center">
-    
+  <q-page>
+    <div class="col-12" :class="viewScreen ? 'q-pa-none' : 'q-pa-xl'">
+      <div
+        v-show="!viewScreen"
+        style="
+          color: red;
+          font-size: 120px;
+          height: fit-content;
+          margin: 0px 0px -60px 30px;
+        "
+      >
+        News
+      </div>
+      <div
+        class="row"
+        style="background-color: red; height: 320px; border-radius: 20px"
+      >
+        <div :class="viewScreen ? 'newsMobile' : 'newsDesktop'">
+          <div
+            v-show="$q.screen.lt.md"
+            style="font-size: 16px; height: fit-content"
+          >
+            News
+          </div>
+          <div :class="viewScreen ? 'newsTitleMobile' : 'newsTitleDesktop'">
+            Lorem Ipsum
+          </div>
+          <div :class="viewScreen ? 'newsContentMobile' : 'newsContentDesktop'">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
+            Suspendisse ullamcorper egestas augue, in fringilla ipsum<br />
+            condimentum at.
+          </div>
+        </div>
+        <div class="col-6">
+          <q-img
+            class="q-pb-xl absolute"
+            src="src/assets/frame1.svg"
+            style="
+              border-radius: 20px;
+              height: 500px;
+              width: 420px;
+              left: 700px;
+              bottom: 10px;
+            "
+          />
+          <div
+            class="absolute q-px-lg"
+            flat
+            style="
+              background-color: #ffffff;
+              box-shadow: 0 0, -1px 0 10px rgb(0, 0, 0, 0.1);
+              border-radius: 30px;
+              bottom: 75px;
+            "
+          >
+            <div style="margin-bottom: -30px">
+              <div
+                style="font-size: 20px"
+                class="full-width flex justify-center"
+              >
+                Learn from best
+              </div>
+              <div
+                style="font-size: 20px"
+                class="full-width flex justify-center"
+              >
+                <span style="color: #96c67f">instructors</span>&nbsp; around
+              </div>
+              <div
+                style="font-size: 20px"
+                class="full-width flex justify-center"
+              >
+                the globe
+              </div>
+            </div>
+            <div class="flex justify-center">
+              <q-img
+                src="src/assets/frame2.svg"
+                style="height: 50px; width: 50px"
+              />
+              <q-img
+                src="src/assets/frame2.svg"
+                style="height: 50px; width: 50px; margin-left: -20px"
+              />
+              <q-img
+                src="src/assets/frame2.svg"
+                style="height: 50px; width: 50px; margin-left: -20px"
+              />
+              <q-img
+                src="src/assets/frame2.svg"
+                style="height: 50px; width: 50px; margin-left: -20px"
+              />
+              <q-img
+                src="src/assets/frame2.svg"
+                style="height: 50px; width: 50px; margin-left: -20px"
+              />
+            </div>
+          </div>
+          <div
+            class="absolute flex flex-center column q-pa-md"
+            flat
+            style="
+              background-color: #ffffff;
+              border-radius: 30px;
+              bottom: 30px;
+              right: 80px;
+            "
+          >
+            <div
+              style="
+                background-color: #e4f9db;
+                border-radius: 30px;
+                width: fit-content;
+              "
+              class="flex justify-center q-pa-sm"
+            >
+              <q-icon name="person" color="positive" />
+            </div>
+            <div
+              class="flex justify-center"
+              style="font-size: 24px; color: #96c67f"
+            >
+              15K
+            </div>
+            <div class="flex justify-center" style="font-size: 12px">
+              Amazing students<br />
+              around the globe
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
-<script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
+<script setup>
+import { useQuasar } from "quasar";
+import { computed } from "vue";
+const $q = useQuasar();
+const viewScreen = computed(() => {
+  return $q.screen.lt.md;
+});
 </script>
+
+<style>
+.newsMobile {
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+  align-self: center;
+  font-weight: bold;
+}
+.newsDesktop {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  font-weight: bold;
+}
+.newsTitleDesktop {
+  font-size: 60px;
+  height: fit-content;
+  font-weight: bold;
+  padding-left: 30px;
+}
+
+.newsTitleMobile {
+  font-size: 20px;
+  height: fit-content;
+  font-weight: bold;
+  padding-left: 30px;
+}
+.newsContentDesktop {
+  height: fit-content;
+  font-size: 16px;
+  padding-left: 30px;
+}
+.newsContentMobile {
+  height: fit-content;
+  font-size: 14px;
+  padding-left: 30px;
+}
+</style>
